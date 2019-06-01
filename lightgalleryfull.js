6e1e35f94b0418a77a701ac018613c28ec7e3ce0
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2019-06-01 18:40:42 +0000

    Update lightgalleryfull.js

--- lightgalleryfull.js
+++ lightgalleryfull.js
@@ -3,9 +3,7 @@ $(window).on("load", function() {
     $.getScript("//cdn.jsdelivr.net/npm/lightgallery@1/dist/js/lightgallery.min.js"),
     $.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),
     $.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),
-	$.getScript("//cdn.jsdelivr.net/npm/lg-share@1/dist/lg-share.min.js"),
-	$.getScript("//cdn.jsdelivr.net/npm/lg-video@1/dist/lg-video.min.js"),
-	$.getScript("//cdn.jsdelivr.net/npm/lg-autoplay@1/dist/lg-autoplay.min.js"),
+    $.getScript("//cdn.jsdelivr.net/npm/lg-share@1/dist/lg-share.min.js"),
     $.Deferred(function(deferred) {
       $(deferred.resolve);
     })
