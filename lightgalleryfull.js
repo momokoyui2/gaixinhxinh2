c6bcbb438bc7b1ef04e03719229e93e84411666f
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2019-06-01 17:30:45 +0000

    Update lightgalleryfull.js

--- lightgalleryfull.js
+++ lightgalleryfull.js
@@ -1,1 +1,18 @@
-$(window).on("load",function(){$.when($.getScript("//cdn.jsdelivr.net/npm/lightgallery@1/dist/js/lightgallery.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),$.getScript("//cdn.jsdelivr.net/npm/lg-share@1/dist/lg-share.min.js"),$.Deferred(function(n){$(n.resolve)})).done(function(){$("#lightgallery").lightGallery({selector:"a[imageanchor]"})})});
+$(window).on("load", function() {
+  $.when(
+    $.getScript("//cdn.jsdelivr.net/npm/lightgallery@1/dist/js/lightgallery.min.js"),
+    $.getScript("//cdn.jsdelivr.net/npm/lg-zoom@1/dist/lg-zoom.min.js"),
+    $.getScript("//cdn.jsdelivr.net/npm/lg-thumbnail@1/dist/lg-thumbnail.min.js"),
+	$.getScript("//cdn.jsdelivr.net/npm/lg-share@1/dist/lg-share.min.js"),
+	$.getScript("//cdn.jsdelivr.net/npm/lg-video@1/dist/lg-video.min.js"),
+	$.getScript("//cdn.jsdelivr.net/npm/lg-autoplay@1/dist/lg-autoplay.min.js"),
+    $.Deferred(function(deferred) {
+      $(deferred.resolve);
+    })
+  ).done(function() {
+    $('#lightgallery').lightGallery({
+      selector: "a[imageanchor]"
+    });
+
+  });
+})
