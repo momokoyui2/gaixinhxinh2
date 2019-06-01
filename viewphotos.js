3e03213c1ecb744f6cdb997b7875aff221c92ac8
momokoyui2 <34786849+momokoyui2@users.noreply.github.com>
2019-06-01 17:32:37 +0000

    Update viewphotos.js

--- viewphotos.js
+++ viewphotos.js
@@ -1,1 +1,31 @@
-jQuery(document).ready(function(e){jQuery("pre view").before("<div class='viewheader'><a class='view-text' data-clipboard-target='pre view' data-clipboard-action='view'>Click To See Full Size Images</a></div>"),e(".view-text").tooltip({trigger:"click"})});var clipboard=new ClipboardJS(".view-text",{target:function(e){return e.parentNode.nextElementSibling}});function setTooltip(e,i){$(e).tooltip("hide").attr("data-original-title",i).tooltip("show")}function hideTooltip(e){setTimeout(function(){$(e).tooltip("hide")},1e3)}clipboard.on("success",function(e){var i=$(e.trigger);setTooltip(i,"Copied"),hideTooltip(i)}),clipboard.on("error",function(e){var i=$(e.trigger);setTooltip("Failed"),hideTooltip(i)});
+jQuery(document).ready(function($) {
+    jQuery("pre view").before("<div class='viewheader'><a class='view-text' data-clipboard-target='pre view' data-clipboard-action='view'>Click To See Full Size Images</a></div>");
+    $('.view-text').tooltip({
+        trigger: 'click'
+    })
+});
+var clipboard = new ClipboardJS(".view-text", {
+    target: function(trigger) {
+        return trigger.parentNode.nextElementSibling
+    }
+});
+
+function setTooltip(btn, message) {
+    $(btn).tooltip('hide').attr('data-original-title', message).tooltip('show');
+}
+
+function hideTooltip(btn) {
+    setTimeout(function() {
+        $(btn).tooltip('hide');
+    }, 1000);
+}
+clipboard.on('success', function(e) {
+    var btn = $(e.trigger);
+    setTooltip(btn, 'Copied');
+    hideTooltip(btn);
+});
+clipboard.on('error', function(e) {
+    var btn = $(e.trigger);
+    setTooltip('Failed');
+    hideTooltip(btn);
+});
